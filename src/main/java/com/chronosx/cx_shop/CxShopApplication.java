cc184b3ee6887abdfb6e0eccb73c5681458c59e8
thanh <thanhleX04@gmail.com>
2025-10-10 10:11:58 +0700

    Initail commit

---
+++ src/main/java/com/chronosx/cx_shop/CxShopApplication.java
@@ -0,0 +1,13 @@
+package com.chronosx.cx_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CxShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chronosx/cx_shop/CxShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronosx.cx_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CxShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
