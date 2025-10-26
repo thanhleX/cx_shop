783363bef917bceca260fccf6ac2a5d9e2fd7ccd
thanh <thanhleX04@gmail.com>
2025-10-26 23:33:08 +0700

    be: setup cors

--- cx_shop_be/src/main/java/com/chronosx/cx_shop/configurations/WebSecurityConfig.java
+++ cx_shop_be/src/main/java/com/chronosx/cx_shop/configurations/WebSecurityConfig.java
@@ -2,14 +2,22 @@ package com.chronosx.cx_shop.configurations;
 
 import static org.springframework.http.HttpMethod.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.chronosx.cx_shop.filters.JwtTokenFilter;
 import com.chronosx.cx_shop.models.Role;
@@ -22,6 +30,7 @@ import lombok.experimental.NonFinal;
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
+@EnableWebMvc
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class WebSecurityConfig {
 
@@ -75,6 +84,20 @@ public class WebSecurityConfig {
                             .anyRequest()
                             .authenticated();
                 });
+
+        http.cors(new Customizer<CorsConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CorsConfigurer<HttpSecurity> httpSecurityCorsConfigurer) {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(List.of("*"));
+                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+                configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+                configuration.setExposedHeaders(List.of("x-auth-token"));
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                httpSecurityCorsConfigurer.configurationSource(source);
+            }
+        });
 
         return http.build();
     }
