58d9eb2fdf99a6f81533db7ab72b63da86e1ab11
thanh <thanhleX04@gmail.com>
2025-10-30 14:18:41 +0700

    fe: product list + search

--- cx_shop_fe/src/app/components/home/home.component.ts
+++ cx_shop_fe/src/app/components/home/home.component.ts
@@ -1,10 +1,102 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
+import { environment } from '../../environments/environment';
+import { max } from 'class-validator';
+import { Router } from '@angular/router';
+import { Category } from '../../models/category';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products: Product[] = [];
+  categories: Category[] = [];
+  selectedCategoryId: number = 0;
+  currentPage: number = 1;
+  itemsPerPage: number = 12;
+  pages: number[] = [];
+  totalPages: number = 0;
+  visiblePages: number[] = [];
+  keyword: string = "";
 
+  constructor(private productService: ProductService, private router: Router, private categoryService: CategoryService) { }
+
+  ngOnInit() {
+    this.getProducts(this.keyword, this.selectedCategoryId, this.currentPage, this.itemsPerPage);
+    this.getCategories(1, 100);
+  }
+
+  getCategories(page: number, limit: number) {
+    this.categoryService.getCategories(page, limit).subscribe({
+      next: (categories: Category[]) => {
+        debugger;
+        this.categories = categories;
+      },
+      complete: () => {
+        debugger;
+      },
+      error: (error: any) => {
+        console.error('Error fetching categories:', error);
+      }
+    });
+  }
+
+  searchProducts() {
+    this.currentPage = 1;
+    this.itemsPerPage = 12;
+    debugger
+    this.getProducts(this.keyword, this.selectedCategoryId, this.currentPage, this.itemsPerPage);
+  }
+
+  getProducts(keyword: string, selectedCategoryId: number, page: number, limit: number) {
+    debugger;
+    this.productService.getProducts(keyword, selectedCategoryId, page, limit).subscribe({
+      next: (response: any) => {
+        debugger;
+        response.products.forEach((product: Product) => {
+          debugger;
+          product.url = `${environment.apiBaseUrl}/products/images/${product.thumbnail}`
+        });
+        this.products = response.products;
+        this.totalPages = response.totalPages;
+        this.visiblePages = this.generateVisiblePageArray(this.currentPage, this.totalPages);
+      },
+      complete: () => {
+        debugger;
+      },
+      error: (err: any) => {
+        debugger;
+        console.error('Error fetching products: ', err);
+      }
+    });
+  }
+
+  onPageChange(page: number) {
+    debugger;
+    this.currentPage = page;
+    this.getProducts(this.keyword, this.selectedCategoryId, this.currentPage, this.itemsPerPage);
+  }
+
+  generateVisiblePageArray(currentPage: number, totalPages: number): number[] {
+    const maxVisiblePages = 5;
+    const halfVisiblePages = Math.floor(maxVisiblePages / 2);
+
+    let startPage = Math.max(currentPage - halfVisiblePages, 1);
+    let endPage = Math.min(startPage + maxVisiblePages - 1, totalPages);
+
+    if (endPage - startPage + 1 < maxVisiblePages) {
+      startPage = Math.max(endPage - maxVisiblePages + 1, 1);
+    }
+
+    return new Array(endPage - startPage + 1).fill(0).map((_, index) => startPage + index);
+  }
+
+  onProductClick(productId: number) {
+    debugger;
+    this.router.navigate(['/detail-product', productId]);
+  }
 }
---
+++ cx_shop_fe/src/app/services/product.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiGetProducts = `${environment.apiBaseUrl}/products`;
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(keyword: string, categoryId: number, page: number, limit: number): Observable<Product[]> {
+    const params = new HttpParams()
+      .set('keyword', keyword)
+      .set('category_id', categoryId)
+      .set('page', page.toString())
+      .set('limit', limit.toString());
+    return this.http.get<Product[]>(this.apiGetProducts, { params });
+  }
+}
